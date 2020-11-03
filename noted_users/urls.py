23f587d70b7029e9c453fdc3e3320b8284a4237d
Jesus Alvarez <alv.mtz94@gmail.com>
2020-11-03 15:44:39 -0700

    added sanity check endpoint

--- noted_users/urls.py
+++ noted_users/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
-from .users.views import UserViewSet, UserCreateViewSet
+from .users.views import UserViewSet, UserCreateViewSet, sanity_check
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -20,6 +20,8 @@ router.register(r'users', UserCreateViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
+
+    path('api/health/', sanity_check), # url used to check API sanity
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- noted_users/users/views.py
+++ noted_users/users/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import ( api_view, 
+    authentication_classes, permission_classes )
+from rest_framework.response import Response
 from .models import User
 from .permissions import IsUserOrReadOnly
 from .serializers import CreateUserSerializer, UserSerializer
@@ -27,3 +30,13 @@ class UserCreateViewSet(mixins.CreateModelMixin,
     queryset = User.objects.all()
     serializer_class = CreateUserSerializer
     permission_classes = (AllowAny,)
+
+
+@api_view(['GET'])
+@permission_classes([])
+@authentication_classes([])
+def sanity_check(request):
+    """ View used to check API status without need of
+        permissions nor authentication
+    """
+    return Response({"healthy": True}, status=200)
